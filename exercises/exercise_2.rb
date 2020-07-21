117a3875394b55603ed1f9fee16586ff9aa84763
landon tipantiza <landontipantiza@gmail.com>
2020-07-21 01:19:11 +0000

    created 3 stores and then updated first stores name

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,11 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store1.update(name: "landon")
+# @store1.name = 'Dave'
+# @store1.save
+puts @store1.name
+
+@store2 = Store.find_by(id: 2)
+puts @store2.name
--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,5 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+
+
