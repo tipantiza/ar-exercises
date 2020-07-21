f02673c9b3ebf28d699e9c3ce806e4f8f8e25e09
landon tipantiza <landontipantiza@gmail.com>
2020-07-21 02:53:43 +0000

    Exercise 6: One-to-many association

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -6,10 +6,11 @@ puts "----------"
 # Your code goes below here ...
 
 class Store < ActiveRecord::Base
+  has_many :employees
 end
 
-Store.create({:name => "Burnaby", :annual_revenue => 300000, :mens_apparel => true, :womens_apparel => true})
-Store.create({:name => "Richmond", :annual_revenue => 1260000, :mens_apparel => false, :womens_apparel => true})
-Store.create({:name => "Gastown", :annual_revenue => 190000, :mens_apparel => true, :womens_apparel => false})
+@store1 = Store.create({:name => "Burnaby", :annual_revenue => 300000, :mens_apparel => true, :womens_apparel => true})
+@store2 = Store.create({:name => "Richmond", :annual_revenue => 1260000, :mens_apparel => false, :womens_apparel => true})
+@store3 = Store.create({:name => "Gastown", :annual_revenue => 190000, :mens_apparel => true, :womens_apparel => false})
 
 puts Store.count
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,18 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Landon", last_name: "Tipantiza", hourly_rate: 100)
+@store1.employees.create(first_name: "Rheema", last_name: "Timmer", hourly_rate: 300)
+
+@store2.employees.create(first_name: "Jalayna", last_name: "Tipantiza", hourly_rate: 10)
+@store2.employees.create(first_name: "Jalysa", last_name: "Cancino", hourly_rate: 20)
+@store2.employees.create(first_name: "German", last_name: "Tipantiza", hourly_rate: 100)
+
+@store4.employees.create(first_name: "Felipe", last_name: "Arias", hourly_rate: 60)
+@store4.employees.create(first_name: "Carlos", last_name: "Cruz", hourly_rate: 60)
+@store4.employees.create(first_name: "Nina", last_name: "Stubs", hourly_rate: 30)
